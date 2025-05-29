4c78535fb37afc1c3f9c5cec2a8b67ca901c66cd
wetrustinprize <me@wetrustinprize.com>
2025-05-28 21:18:04 -0300

    feat: logout

--- src/client/layouts/system.tsx
+++ src/client/layouts/system.tsx
@@ -1,5 +1,26 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
+import { MdLogout } from "react-icons/md";
+import { Button } from "@heroui/button";
+import Cookies from "js-cookie";
 
 export default function SystemLayout() {
-    return (<Outlet/>);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        Cookies.remove("jwt");
+        navigate("/");
+    };
+
+    return (
+        <section className="p-4 flex gap-2 h-screen w-screen">
+            <div className="h-auto p-4 rounded-xl bg-neutral-200 w-3xs">
+                <Button isIconOnly color="danger" onPress={handleLogout}>
+                    <MdLogout />
+                </Button>
+            </div>
+            <div className="w-auto h-auto flex-1">
+                <Outlet />
+            </div>
+        </section>
+    );
 }
